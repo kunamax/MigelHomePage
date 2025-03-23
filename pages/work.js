70393f381ec1394da0b919c7dcf447720d155214
Jakub Sadkiewicz <jsadkiewicz@student.agh.edu.pl>
2025-03-23 21:58:42 +0100

    chores: formatting

--- pages/work.js
+++ pages/work.js
@@ -1,22 +1,43 @@
-import {Container, Heading, SimpleGrid } from '@chakra-ui/react'
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 import Layout from '../components/layouts/article'
 
 import rougeLikeGame from '../public/images/game.png'
+import miciuPhotoShop from '../public/images/miciu_piciu.png'
 
 const Work = () => {
   return (
     <Layout>
       <Container>
-        <Heading as={"h2"} fontSize={20} mb={4}>
+        <Heading as={'h2'} fontSize={20} mb={4}>
           Projects
         </Heading>
 
-        <SimpleGrid columns={[1,1,2]} gap={6}>
+        <SimpleGrid columns={[2, 2, 2]} gap={6}>
           <Section>
-            <WorkGridItem id="game" title="Shadow's Labyrinth" thumbnail={rougeLikeGame} link={"https://github.com/kunamax/PythonProject"}>
-              Shadow&apos;s Labyrinth is a rouge-like game that I developed with my friend. It is a turn-based game where you have to navigate through a maze and defeat enemies.
+            <WorkGridItem
+              id="game"
+              title="Shadow's Labyrinth"
+              thumbnail={rougeLikeGame}
+              link={'https://github.com/kunamax/PythonProject'}
+            >
+              Shadow&apos;s Labyrinth is a rouge-like game that I developed with
+              my friend. It is a turn-based game where you have to navigate
+              through a maze and defeat enemies.
+            </WorkGridItem>
+          </Section>
+          <Section>
+            <WorkGridItem
+              id="cpp"
+              title="Miciu(Photo)Shop"
+              thumbnail={miciuPhotoShop}
+              link={'https://github.com/kunamax/CppProject'}
+            >
+              Miciu(Photo)Shop is a photo editing software. It is a simple photo
+              editing software that allows you to apply filters and
+              transformations to images. I used C++, OpenCV and Qt to develop
+              this project.
             </WorkGridItem>
           </Section>
         </SimpleGrid>
